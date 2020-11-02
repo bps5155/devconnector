718e2999b2911615442474c03ad9b90af49ff4e1
Benjamin Susko <ben.susko@gmail.com>
2020-11-02 11:59:33 -0500

    first commit

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+//mongoose.connect(db);
+const connectDB = async () => { //most cases should wrap async await in try catch block
+    try{
+        await mongoose.connect(db,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+
+        console.log("MongoDB Connected...");
+    } catch(err) {
+        console.error(err.message);
+        //Exit process with failure
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;  //does this export only the one function or the whole file?
