5ab80109073bbe79851a0ab899f1b78fcc1d4505
Benjamin Susko <ben.susko@gmail.com>
2020-11-11 16:03:10 -0500

    added front end files and started front end

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
+
+const App = () => (
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path = "/" component = { Landing } />
+    </Fragment>
+  </Router>
+);
+export default App;
